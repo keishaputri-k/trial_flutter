e4e113596cc5869c98224bac0ddc70dc6fcfba48
keishaputritheanny <pkeisha47@gmail.com>
2021-07-29 11:17:52 +0700

    Style Variable

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     var name = 'Ivan';
+    var _titleStyle = TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 20,
+                      // color: Color.fromRGBO(66, 165, 245, 0.7)
+                      color: Colors.blue[200]
+                );
 
     return MaterialApp(
       title: 'Welcome To Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome To Flutter'),
-        ),
+      home:Scaffold
+        (appBar: AppBar(
+        title: Text('Welcome To Flutter'),
+      ),
         body: Center(
-          child: Text.rich(
-            TextSpan(
-              text: 'Hello', //default text style
-              children: <TextSpan>[
-                TextSpan(
-                    text: 'beautiful',
-                    style: TextStyle(fontStyle: FontStyle.italic)),
-                TextSpan(
-                    text: 'world',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-              ],
+            child: Text(
+            'Halo $name!',
+            style: _titleStyle,
             ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
